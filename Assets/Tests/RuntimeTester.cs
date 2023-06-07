73fe97d75dfb3c975346cbeb4e3404767fb0ee81
Paul FIGIEL <pf@homagames.com>
2023-06-07 17:07:48 +0200

    Init

---
+++ Assets/Tests/RuntimeTester.cs
@@ -0,0 +1,38 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.TestTools.TestRunner.Api;
+using UnityEngine;
+
+public class RuntimeTester : ICallbacks
+{
+    [MenuItem("Homa/test")]
+    public static void Test()
+    {
+        var testRunnerApi = ScriptableObject.CreateInstance<TestRunnerApi>();
+        var filter = new Filter()
+        {
+            testMode = TestMode.PlayMode,
+            targetPlatform = BuildTarget.Android
+        };
+        testRunnerApi.Execute(new ExecutionSettings(filter));
+        testRunnerApi.RegisterCallbacks(new RuntimeTester());
+    }
+
+    public void RunStarted(ITestAdaptor testsToRun)
+    {
+    }
+
+    public void RunFinished(ITestResultAdaptor result)
+    {
+        Debug.Log($"Results : {result.ToXml().OuterXml}");
+    }
+
+    public void TestStarted(ITestAdaptor test)
+    {
+    }
+
+    public void TestFinished(ITestResultAdaptor result)
+    {
+    }
+}
+#endif
