3d5a82f6c6ce9278d098c059dd973902b3b2f427
Paul FIGIEL <pf@homagames.com>
2023-06-08 10:38:36 +0200

    Test

--- Assets/Tests/RuntimeTester.cs
+++ Assets/Tests/RuntimeTester.cs
@@ -1,4 +1,5 @@
 #if UNITY_EDITOR
+using System;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEditor.TestTools.TestRunner.Api;
@@ -14,9 +15,13 @@ public class RuntimeTester : ICallbacks
         var filter = new Filter()
         {
             testMode = TestMode.PlayMode,
-            targetPlatform = BuildTarget.StandaloneOSX
+            targetPlatform = BuildTarget.Android
         };
-        Debug.Log(testRunnerApi.Execute(new ExecutionSettings(filter)));
+        testRunnerApi.Execute(new ExecutionSettings(filter)
+        {
+            runSynchronously = true,
+            playerHeartbeatTimeout = Int32.MaxValue
+        });
         var tester = new RuntimeTester();
         testRunnerApi.RegisterCallbacks(tester);
     }
@@ -28,6 +33,7 @@ public class RuntimeTester : ICallbacks
     public void RunFinished(ITestResultAdaptor result)
     {
         Debug.Log($"Results : {result.ToXml().OuterXml}");
+        EditorApplication.Exit(0);
     }
 
     public void TestStarted(ITestAdaptor test)
